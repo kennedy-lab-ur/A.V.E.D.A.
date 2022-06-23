24eda01ed4f901c41b8ac05a7523716afb9b4f2f
Dalton Hanaway <dhanaway@u.rochester.edu>
2022-06-23 09:43:19 -0700

    changes permissions

--- AVEDA/Program/6_electricFieldOpt/stepRestarter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curMag=`cat ./lvl.txt`
-typeStruct=`cat ./struct.txt`
-
-sh ./stepErrorSubmissionScripter.sh "${typeStruct}_zmat_field_N${curMag}" "lastPoint_initalOptXYZ_zmat" "${typeStruct}_N${curMag}.txt" ${typeStruct}
