8e742de3f096ebd33d8f8edfe16274c9440cb049
Dalton Hanaway <dhanaway@u.rochester.edu>
2022-07-19 13:29:53 -0600

    Update start.sh

--- AVEDA/start.sh
+++ AVEDA/start.sh
@@ -15,56 +15,64 @@ echo " "
 echo "Beginning processes for A.V.E.D.A." 
 echo " "
 
-echo "## Starting A.V.E.D.A. for ${8} ## " >> Report_${8}.txt
-echo " " >> Report_${8}.txt
+if [ -z "${10}" ]
+then
+	echo "Not all arguments satisfied for A.V.E.D.A. please check input"
+	echo "Run Terminated"
+else 
+	echo "## Starting A.V.E.D.A. for ${8} ## " >> Report_${8}.txt
+	echo " " >> Report_${8}.txt
 
-cp -r ./Program/ ./${8}
-echo "Created job specific directory"
+	cp -r ./Program/ ./${8}
+	echo "Created job specific directory"
 
-# makes directory to hold input specifications
-mkdir ./${8}/inputParameters/
+	# makes directory to hold input specifications
+	mkdir ./${8}/inputParameters/
 
-echo ${3} >> ./${8}/inputParameters/charge.txt
-echo ${4} >> ./${8}/inputParameters/multi.txt
-echo ${5} >> ./${8}/inputParameters/func.txt
-echo ${6} >> ./${8}/inputParameters/basis.txt
+	echo ${3} >> ./${8}/inputParameters/charge.txt
+	echo ${4} >> ./${8}/inputParameters/multi.txt
+	echo ${5} >> ./${8}/inputParameters/func.txt
+	echo ${6} >> ./${8}/inputParameters/basis.txt
 
-echo ${7} >> ./${8}/inputParameters/reorder.txt
-echo ${8} >> ./${8}/inputParameters/name.txt
+	echo ${7} >> ./${8}/inputParameters/reorder.txt
+	echo ${8} >> ./${8}/inputParameters/name.txt
 
-echo ${9} >> ./${8}/inputParameters/numProcess.txt
+	echo ${9} >> ./${8}/inputParameters/numProcess.txt
 
-echo ${9} >> ./${8}/inputParameters/memTotalGauss.txt
+	echo ${9} >> ./${8}/inputParameters/memTotalGauss.txt
 
-echo ${10} >> ./${8}/inputParameters/partition.txt
+	echo ${10} >> ./${8}/inputParameters/partition.txt
 
-sinfo -p ${10} -h -o=%l | cut -c2- >> ./${8}/inputParameters/maximumTime.txt
- 	
-echo " Optimizing intermediate and transition state" >> Report_${8}.txt
+	sinfo -p ${10} -h -o=%l | cut -c2- >> ./${8}/inputParameters/maximumTime.txt
+	 	
+	echo " Optimizing intermediate and transition state" >> Report_${8}.txt
 
-# Move into program directory and begin optimizations
-cp ./${1} ./${8}/1_optimization/"ts.xyz"
-cp ./${2} ./${8}/1_optimization/"int.xyz"
+	# Move into program directory and begin optimizations
+	cp ./${1} ./${8}/1_optimization/"ts.xyz"
+	cp ./${2} ./${8}/1_optimization/"int.xyz"
 
-mkdir ./${8}/inputGeomFiles/
-mv ./${1} ./${8}/inputGeomFiles/
-mv ./${2} ./${8}/inputGeomFiles/
+	mkdir ./${8}/inputGeomFiles/
+	mv ./${1} ./${8}/inputGeomFiles/
+	mv ./${2} ./${8}/inputGeomFiles/
 
-cd ./${8}/1_optimization/
+	cd ./${8}/1_optimization/
 
-sh ./submissionScripter.sh "ts" 0 ${3} ${4} ${5} ${6} ${9} 
-sh ./submissionScripter.sh "int" 1 ${3} ${4} ${5} ${6} ${9}
+	sh ./submissionScripter.sh "ts" 0 ${3} ${4} ${5} ${6} ${9} 
+	sh ./submissionScripter.sh "int" 1 ${3} ${4} ${5} ${6} ${9}
 
 
-for i in *.gjf; do
- 
- 	./submitGaussian.sh ${i%%.gjf}
+	for i in *.gjf; do
+	 
+	 	./submitGaussian.sh ${i%%.gjf}
 	
-done
+	done
 
 
-echo " "
-echo "Inital submissions complete,"
-echo "A.V.E.D.A.'s progress will be logged in Report_${8}.txt"
+	echo " "
+	echo "Inital submissions complete,"
+	echo "A.V.E.D.A.'s progress will be logged in Report_${8}.txt"
+
+
+fi
 
 
